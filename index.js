fc67c410a004e3c9faf15a0dd137feb29cf6464f
iqbal rahman <iqbal.rahman.pens@gmail.com>
2023-06-12 19:01:59 +0700

    2. Update mongo db atlas

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ import Router from "./routes/routes.js";
 // init express
 const app = express();
 
-mongoose.connect("mongodb://0.0.0.0:27017/peoject1", {
+mongoose.connect("mongodb+srv://iqbal070103:Iqbal2507@transprima.lohqzt6.mongodb.net/project1", {
     useNewUrlParser: true,
     useUnifiedtopology: true
 });
