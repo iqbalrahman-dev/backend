dfc64ee6cd6bc93f9c057e479d97959386a70bd2
iqbal rahman <iqbal.rahman.pens@gmail.com>
2023-06-12 19:59:16 +0700

    4. Update index

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ import Router from "./routes/routes.js";
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+mongoose.set("strictQuery", false);
 mongoose.connect(process.env.MONGO_URI, {
     useNewUrlParser: true,
     useUnifiedtopology: true
